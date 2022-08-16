d7ce9e1f84d2c9a2f9f58ad6a901d6c6f6b0f486
kojiyama1969 <kojiyama1969@gmail.com>
2022-08-16 22:27:14 +0900

    Update convertgpx.js
    convert gpx created rwgps to cnx file

--- convertgpx.js
+++ convertgpx.js
@@ -1,132 +1,228 @@
 
-      function convertgpx(event){
-	  // this is for 8.5.5
-	  // var typeTable = { "Left":"左", "Right":"右", "Slight Left":"やや左", "Slight Right":"やや右", "Sharp Left":"左に急カーブ", "Sharp Right":"右に急カーブ", "Straight":"そのまま進行", "Uturn":"リターン", "Summit":"頂上", "Water":"水飲み場", "Food":"レストラン", "Danger":"危険地帯", "First Aid":"救急車", "Control":"情報案内", "Generic":"ポイント", "Sprint":"写真"};
-	  // this is for 7.x
-	  var typeTable = { "Left":"左", "Right":"右", "Slight Left":"Slight Left", "Slight Right":"Slight Right", "Sharp Left":"Turn Sharp Left", "Sharp Right":"Turn Sharp Right", "Straight":"そのまま進行", "Uturn":"リターン", "Summit":"頂上", "Water":"水飲み場", "Food":"レストラン", "Danger":"危険地帯", "First Aid":"Ambulance", "Control":"情報案内", "Generic":"ポイント", "Sprint":"写真"};
-	  var items = document.getElementById("download_item");
-	  var li = document.createElement("li");
-	  var serializer = new XMLSerializer();
-	  var modifyFlag = false;
-	  var dom = new DOMParser();
-	  var doc = dom.parseFromString(event.target.result,"application/xml");
-	  var e = doc.firstChild;
-	  if(e != null && e.tagName == "gpx"){
-	      var elements = doc.getElementsByTagName("wpt");
-	      for(var e of elements){
-		  var descs = e.getElementsByTagName("desc");
-		  if(descs.length > 0){
-		      var names = e.getElementsByTagName("name");
-		      if(names.length > 0){
-			  var sym = doc.createElementNS(e.namespaceURI, "sym");
-			  sym.textContent = "Waypoint";
-			  var type = doc.createElementNS(e.namespaceURI, "type");
-			  if(names[0].textContent in typeTable){
-			      type.textContent = typeTable[names[0].textContent];
-			  }else{
-			      type.textContent = "Dot";
-			  }
-			  var newName = doc.createElementNS(e.namespaceURI, "name");
-			  newName.textContent = descs[0].textContent;
-			  e.replaceChild(newName,names[0]);
+function convertgpx(event) {
+	var templateString = `<?xml version="1.0" encoding="utf-8"?>
+<Route>
+  <Id>0</Id>
+  <Distance>0</Distance>
+  <Duration>0</Duration>
+  <Ascent>0.0</Ascent>
+  <Descent>0.0</Descent>
+  <Encode>0</Encode>
+  <Lang>0</Lang>
+  <TracksCount>0</TracksCount>
+  <Tracks></Tracks>
+  <Navs />
+  <Points>
+  </Points>
+</Route>
+`;
+/*
+	waypoint definition for cnx
+	0: ウェイポイント
+	1: スプリントポイント
+	2: ヒルクライム
+	3: レベル1クライミング
+	4: レベル2クライミング
+	5: レベル3クライミング
+	6: レベル4クライミング
+	7: サプリポイント（食事）
+	8: ゴミ収集場
+	9: トイレ
+	10: サービスポイント
+	11: 医療援助ステーション
+	12: 装備エリア
+	13: ショップ
+	14: 集合ポイント
+	15: 展望台
+	16: インスタ映え場所
+	17: トンネル
+	18: 谷
+	19: 危険な道路
+	20: 急カーブ
+	21: 急斜面
+	22: 交差点
+*/
+	var typeTable = {
+		"Left": 0,
+		"Right": 0,
+		"Slight Left": 0,
+		"Slight Right": 0,
+		"Sharp Left": 0,
+		"Sharp Right": 0,
+		"Straight": 0,
+		"Uturn": 0,
+		"Summit": 15,
+		"Valley": 18,
+		"Water": 10,
+		"Food": 7,
+		"Danger": 19,
+		"First Aid": 11,
+		"Control": 14,
+		"Climb": 2,
+		"4th Category": 6,
+		"3rd Category": 5,
+		"2nd Category": 4,
+		"1st Category": 3,
+		"Hors Category": 2,
+		"Sprint": 1,
+		"Generic": 16
+	 };
+	var items = document.getElementById("download_item");
+	var li = document.createElement("li");
+	var serializer = new XMLSerializer();
+	var modifyFlag = false;
+	var dom = new DOMParser();
+	var newDoc = dom.parseFromString(templateString, 'application/xml');
+//	var newDoc = document.implementation.createDocument('','',null);
+	var doc = dom.parseFromString(event.target.result, "application/xml");
+	var e = doc.firstChild;
+	var tracksCount = 0;
+	var tracks = '';
+	if (e != null && e.tagName.toUpperCase() === 'GPX') {
+		var elements = doc.getElementsByTagName('trkpt');
+		for (var e of elements) {
+			var eleElements = e.getElementsByTagName('ele');
+			if (eleElements.length > 0) {
+				var lat = e.getAttribute('lat');
+				var lon = e.getAttribute('lon');
+				var ele = Math.round(Number(eleElements[0].textContent) * 100);
+				tracks += `${lat},${lon},${ele};`;
+				tracksCount++;
+			}
+		}
+		var tracksCountElements = newDoc.getElementsByTagName('TracksCount');
+		if(tracksCountElements.length > 0){
+			tracksCountElements[0].textContent = tracksCount;
+		}
+		var tracksElements = newDoc.getElementsByTagName('Tracks');
+		if(tracksElements.length > 0){
+			tracksElements[0].textContent = tracks;
+		}
 
-			  var oldSym = e.getElementsByTagName("sym");
-			  if(oldSym.length > 0){
-			      e.replaceChild(sym,oldSym[0]);
-			  }
-			  var oldType = e.getElementsByTagName("type");
-			  if(oldType.length > 0){
-			      e.replaceChild(type,oldType[0]);
-			  }
+		var newPointsElements = newDoc.getElementsByTagName('Points');
+		if(newPointsElements.length > 0){
+			var elements = doc.getElementsByTagName('wpt');
+			for (var e of elements){
+				var descr = '';
+				var pointType = 0;
+				var cmtElements = e.getElementsByTagName('cmt');
+				var nameElements = e.getElementsByTagName('name');
+				var lon = e.getAttribute('lon');
+				var lat = e.getAttribute('lat');
+				if(cmtElements.length > 0){
+					descr = cmtElements[0].textContent;
+					if(nameElements.length > 0){
+						if(nameElements[0].textContent in typeTable){
+							pointType = typeTable[nameElements[0].textContent];
+						}
+					}
+				}else{
+					if(nameElements.length > 0){
+						descr = nameElements[0].textContent;
+					}
+				}
+				var pointElement = newDoc.createElement('Point');
+				var lonElement = newDoc.createElement('Lng');
+				lonElement.textContent = lon;
+				var latElement = newDoc.createElement('Lat');
+				latElement.textContent = lat;
+				var typeElement = newDoc.createElement('Type');
+				typeElement.textContent = pointType;
+				var descrElement = newDoc.createElement('Descr');
+				descrElement.textContent = descr;
+				pointElement.appendChild(latElement);
+				pointElement.appendChild(lonElement);
+				pointElement.appendChild(typeElement);
+				pointElement.appendChild(descrElement);
+				newPointsElements[0].appendChild(pointElement);
+			}
+		}
+		modifyFlag = true;
 
-			  modifyFlag = true;
-		      }
-		  }			  
-	      }
-	      if(modifyFlag){
-		  var img = document.createElement("img");
-		  img.src = "img/GPX_icon-vector.svg";
-		  var output = serializer.serializeToString(doc);
-		  var blob = new Blob([output], {type: 'application/gpx+xml'});
-		  var url = URL.createObjectURL(blob);
-		  var a = document.createElement("a");
-		  a.download = this.file.name;
-		  a.href = url;
-		  a.appendChild(img);
-		  a.appendChild(document.createTextNode(this.file.name));
-		  li.appendChild(a);
-	      }else{
-		  li.textContent = this.file.name + " is NOT modified.";
-	      }
-	  }else{
-	      li.textContent = this.file.name + " is NOT a gpx file.";
-	  }
-	  items.appendChild(li);
-      }
+		if (modifyFlag) {
+			var img = document.createElement("img");
+			img.src = "img/GPX_icon-vector.svg";
+			var output = serializer.serializeToString(newDoc);
+			var blob = new Blob([output], { type: 'application/gpx+xml' });
+			var url = URL.createObjectURL(blob);
+			var a = document.createElement("a");
+			a.download = this.file.name+".cnx";
+			a.href = url;
+			a.appendChild(img);
+			a.appendChild(document.createTextNode(this.file.name+".cnx"));
+			li.appendChild(a);
+		} else {
+			li.textContent = this.file.name + " is NOT modified.";
+		}
+	} else {
+		li.textContent = this.file.name + " is NOT a gpx file.";
+	}
+	items.appendChild(li);
+}
 
-      function errorgpx(event){
-	  var items = document.getElementById("download_item");
-	  var li = document.createElement("li");
-	  li.textContent = this.file.name + " is NOT read.";
-	  items.appendChild(li);
-      }
-      
-      function selectedFiles(files){
-	  if(files.length > 0){
-	      for(var i = 0, f;f = files[i];i++){
-		  var reader = new FileReader();
-		  reader.addEventListener('load',{handleEvent: convertgpx, file: f});
-		  reader.addEventListener('error',{handleEvent: errorgpx, file: f});
-		  reader.readAsText(f);
-	      }
-	  }else{
-	      var items = document.getElementById("download_item");
-	      var li = document.createElement("li");
-	      li.textContent = "It is NOT a file.";
-	      items.appendChild(li);
-	  }
-      }
-      
-      function showDropZoneEnter(){
-	  var dropZone = document.getElementById('drop_zone');
-	  dropZone.style.backgroundColor = 'lightsteelblue';
-      }
-      function showDropZoneLeave(){
-	  var dropZone = document.getElementById('drop_zone');
-	  dropZone.style.backgroundColor = 'aliceblue';
-      }
+function errorgpx(event) {
+	var items = document.getElementById("download_item");
+	var li = document.createElement("li");
+	li.textContent = this.file.name + " is NOT read.";
+	items.appendChild(li);
+}
 
-      function handleFileDrop(evt){
-	  evt.stopPropagation();
-	  evt.preventDefault();
+function selectedFiles(files) {
+	if (files.length > 0) {
+		for (var i = 0, f; f = files[i]; i++) {
+			var reader = new FileReader();
+			reader.addEventListener('load', { handleEvent: convertgpx, file: f });
+			reader.addEventListener('error', { handleEvent: errorgpx, file: f });
+			reader.readAsText(f);
+		}
+	} else {
+		var items = document.getElementById("download_item");
+		var li = document.createElement("li");
+		li.textContent = "It is NOT a file.";
+		items.appendChild(li);
+	}
+}
 
-	  selectedFiles(evt.dataTransfer.files);
-	  showDropZoneLeave();
-      }
-      function handleFileSelect(evt){
-	  selectedFiles(evt.target.files);
-      }
-      function handleDragOver(evt){
-	  evt.stopPropagation();
-	  evt.preventDefault();
-	  evt.dataTransfer.dropEffect = 'copy';
-	  showDropZoneEnter();
-      }
-      function handleDragLeave(evt){
-	  evt.stopPropagation();
-	  evt.preventDefault();
-	  evt.dataTransfer.dropEffect = 'none';
-	  showDropZoneLeave();
-      }
-      function handleDragEnd(evt){
-	  evt.stopPropagation();
-	  evt.preventDefault();
-	  showDropZoneLeave();
-      }
-      
-      document.getElementById('files').addEventListener('change',handleFileSelect);
-      var dropZone = document.getElementById('drop_zone');
-      dropZone.addEventListener('dragover', handleDragOver, false);
-      dropZone.addEventListener('dragleave', handleDragLeave, false);
-      dropZone.addEventListener('dragend', handleDragEnd, false);
-      dropZone.addEventListener('drop', handleFileDrop, false);
+function showDropZoneEnter() {
+	var dropZone = document.getElementById('drop_zone');
+	dropZone.style.backgroundColor = 'lightsteelblue';
+}
+function showDropZoneLeave() {
+	var dropZone = document.getElementById('drop_zone');
+	dropZone.style.backgroundColor = 'aliceblue';
+}
+
+function handleFileDrop(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	selectedFiles(evt.dataTransfer.files);
+	showDropZoneLeave();
+}
+function handleFileSelect(evt) {
+	selectedFiles(evt.target.files);
+}
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'copy';
+	showDropZoneEnter();
+}
+function handleDragLeave(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'none';
+	showDropZoneLeave();
+}
+function handleDragEnd(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	showDropZoneLeave();
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect);
+var dropZone = document.getElementById('drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('dragleave', handleDragLeave, false);
+dropZone.addEventListener('dragend', handleDragEnd, false);
+dropZone.addEventListener('drop', handleFileDrop, false);
